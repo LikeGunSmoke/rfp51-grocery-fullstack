2007f1cf8216d4f7eaf1f75d38bcd674b548d45f
Robert Strange <robertjohn23@live.com>
2021-02-13 12:43:55 -0700

    Implement database test data

--- schema.sql
+++ schema.sql
@@ -8,8 +8,11 @@ CREATE TABLE list (
   quantity INT NOT NULL
 );
 
-INSERT INTO list (1, 'fake_apple', 15)
+INSERT INTO list (id, `name`, quantity) VALUES (1, 'fake apples', 15);
+INSERT INTO list (id, `name`, quantity) VALUES (2, 'fake avocado', 27);
+INSERT INTO list (id, `name`, quantity) VALUES (3, 'fake mango', 9000);
 
 /*  Execute this file from the command line by typing:
- *  mysql -u root < schema.sql
- *  to create the database and the tables.*/
+   - mysql -u root < schema.sql / drop db between tests
+   - DROP DATABASE groceries;
+    to create the database and the tables.*/
